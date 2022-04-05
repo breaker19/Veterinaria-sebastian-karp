921dc04f2b1c91a1e689653f01601ce1ed396ad4
breaker19 <sebakarp26@gmail.com>
2022-04-05 09:21:22 -0300

    products, maps and promises

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { NavBar } from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ItemListContainer from './components/ItemListContainer';
+import React from 'react';
 
 
 
